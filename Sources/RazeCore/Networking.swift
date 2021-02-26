d6bd2b624cf3f4e670612a95c5b18cfbdbe4138c
SmritiArora <smriti.arora@nineleaps.com>
2021-02-27 02:44:24 +0530

    Added the send data method.

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,10 +9,18 @@ import Foundation
 
 public protocol NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?,Error?) -> Void )
+    func post(with url: URL, completionHandler: @escaping (Data?,Error?) -> Void )
 }
 
 extension URLSession: NetworkSession {
     public func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
+    public func post(with url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         let task = dataTask(with: url) { data, _, error in
             completionHandler(data, error)
         }
@@ -37,6 +45,29 @@ extension RazeCore {
                 session.get(from: url) { (data, error) in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
+                }
+            }
+            
+            /// Sends data to url
+            /// - Parameters:
+            ///   - url: the place where the data is to be sent
+            ///   - body: the object intended to send
+            ///   - completionHandler: Returns the result object
+            public func sendData<I: Codable>(to url: URL, body: I, completionHandler: @escaping (NetworkResult<Data>) -> Void ) {
+                session.get(from: url) { (data, error) in
+                    var request = URLRequest(url: url)
+                    do {
+                        let httpBody = try JSONEncoder().encode(data)
+                        request.httpBody = httpBody
+                        request.httpMethod = "POST"
+                        self.session.post(with: url) { (data, error) in
+                            let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                            completionHandler(result)
+                        }
+                    } catch let error{
+                        return completionHandler(.failure(error))
+                    }
+                    
                 }
             }
         }
